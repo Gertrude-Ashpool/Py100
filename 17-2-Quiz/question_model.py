cfea67c35424c5f8e09902324dcca59711b62dc9
G.A Infosys <stumblerips@proton.me>
2024-07-02 13:52:24 +1000

    add 17-2-Quiz

---
+++ 17-2-Quiz/question_model.py
@@ -0,0 +1,7 @@
+# create a Question class
+class Question:
+
+    # initiate attributes to pass text and answer
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer

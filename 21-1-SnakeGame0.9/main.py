e85c5a923a2ce7ea6b45a3ad056b60f79a96bb73
Gertrude-Ashpool <stumblerips@proton.me>
2024-07-09 16:42:00 +1000

    snake0.9

--- 21-1-SnakeGame0.9/main.py
+++ 21-1-SnakeGame0.9/main.py
@@ -1,7 +1,8 @@
-import time
 from turtle import Screen
 from snake import Snake
-
+from food import Food
+from scoreboard import ScoreBoard
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,17 +10,16 @@ screen.bgcolor("black")
 screen.title("Jake the Snake")
 screen.tracer(0)
 
-
-
 # create the initial snake
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, key="w")
 screen.onkey(snake.down, key="s")
 screen.onkey(snake.left, key="a")
 screen.onkey(snake.right, key="d")
-
 
 game_is_on = True
 
@@ -29,9 +29,9 @@ while game_is_on:
 
     snake.move()
 
-
-
-
-
+    # Detect Collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
---
+++ 21-1-SnakeGame0.9/scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align="Center", font=("Courier", 24, "normal"))
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="Center", font=("Courier", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()

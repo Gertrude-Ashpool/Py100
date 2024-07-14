023e15ffad1861729c0783b20d5b4ca60821c246
G.A Infosys <stumblerips@proton.me>
2024-07-15 09:43:12 +1000

    fixes

--- 24-4-MailMerge/main.py
+++ 24-4-MailMerge/main.py
@@ -6,6 +6,8 @@
 NAMES = './Input/Names/invited_names.txt'
 LETTER = './Input/Letters/starting_letter.txt'
 OUTPUT = './Output/ReadyToSend/'
+PLACEHOLDER = '[name]'
+TEXT = "Apples, Bananas"
 
 
 def get_names(filename):
@@ -15,24 +17,18 @@ def get_names(filename):
 
 def get_letter(filename):
     with open(filename) as letter:
-        return letter.readlines()
-
-
-def fill_name(salutation, invitee_name):
-    new_salutation = salutation.replace("[name]", invitee_name)
-    return new_salutation
+        return letter.read()
 
 
 def write_letter(invitee_name):
-    letter_table = get_letter(LETTER)
-    first_line = letter_table[0]
-    letter_table[0] = fill_name(first_line, invitee_name.strip())
+    template = get_letter(LETTER)
+    new_letter = template.replace(PLACEHOLDER, invitee_name)
     with open(f'{OUTPUT}letter_for_{invitee_name}.txt', mode='w') as destination_letter:
-        for line in letter_table:
-            destination_letter.write(line)
+        destination_letter.write(new_letter)
 
 
 names_table = get_names(NAMES)
 
 for name in names_table:
-    write_letter(name)
+    write_letter(name.strip())
+
